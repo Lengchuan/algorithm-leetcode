1bb60dc4892fb7514f50b2e879624c2a9ec2ea29
lengchuan <lishuijun1992@gmail.com>
2020-05-25 23:17:14 +0800

    动态规划---P53 最大子序列和

--- src/com/org/lengchuan/algorithm/leetcode/p53/Solution.java
+++ src/com/org/lengchuan/algorithm/leetcode/p53/Solution.java
@@ -24,7 +24,7 @@ public class Solution {
         //状态转移方程 dp[i] = max(Vi,dp[i-1]+Vi)
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        int max = 0;
+        int max = dp[0];
         for (int i = 1; i < nums.length; i++) {
             dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
             max = Math.max(dp[i], max);
