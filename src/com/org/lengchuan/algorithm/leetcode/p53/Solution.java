0f731b3d243cf1bfa07db26152eac2132e2f0f79
lengchuan <lishuijun1992@gmail.com>
2020-05-25 23:11:42 +0800

    动态规划---P53 最大子序列和

---
+++ src/com/org/lengchuan/algorithm/leetcode/p53/Solution.java
@@ -0,0 +1,39 @@
+package com.org.lengchuan.algorithm.leetcode.p53;
+
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//
+// 示例:
+//
+// 输入: [-2,1,-3,4,-1,2,1,-5,4],
+//输出: 6
+//解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+//
+//
+// 进阶:
+//
+// 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+// Related Topics 数组 分治算法 动态规划
+
+/**
+ * @author lengchuan <lishuijun1992@gmail.com>
+ * @date 2020/5/25
+ * @desc
+ */
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        //状态转移方程 dp[i] = max(Vi,dp[i-1]+Vi)
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new Solution().maxSubArray(nums));
+    }
+}
