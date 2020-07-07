86a96e0a12670a04f81aff937ed986bdc4a847f7
lengchuan <lishuijun1992@gmail.com>
2020-07-07 22:47:47 +0800

    p107 二叉树的层序遍历②

---
+++ src/com/org/lengchuan/algorithm/leetcode/p107/Solution.java
@@ -0,0 +1,92 @@
+package com.org.lengchuan.algorithm.leetcode.p107;
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+//
+//
+//
+// 示例：
+//二叉树：[3,9,20,null,null,15,7],
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回其层次遍历结果：
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+//
+// Related Topics 树 广度优先搜索
+
+import java.util.*;
+
+/**
+ * @author lengchuan <lishuijun1992@gmail.com>
+ * @date 2020/6/19
+ * @desc
+ */
+public class Solution {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        // 广度优先 使用队列辅助
+        List<List<Integer>> res = new ArrayList<>();
+        Stack<List<Integer>> stack = new Stack<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            int i = 0;
+            while (i < size) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                i++;
+            }
+            stack.add(list);
+        }
+
+        while (!stack.isEmpty()) {
+            res.add(stack.pop());
+        }
+
+        return res;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode n9 = new TreeNode(9);
+        TreeNode n20 = new TreeNode(20);
+        TreeNode n15 = new TreeNode(15);
+        TreeNode n17 = new TreeNode(17);
+        root.left = n9;
+        root.right = n20;
+        n20.left = n15;
+        n20.right = n17;
+
+        System.out.println(new Solution().levelOrderBottom(root));
+    }
+}
