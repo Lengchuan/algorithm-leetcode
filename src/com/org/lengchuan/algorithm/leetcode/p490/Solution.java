4afd3fb97d1604149615694ccf2923674a9443d9
lengchuan <lishuijun1992@gmail.com>
2020-08-04 23:14:27 +0800

    p490 迷宫问题--广度优先搜索

--- src/com/org/lengchuan/algorithm/leetcode/p490/Solution.java
+++ src/com/org/lengchuan/algorithm/leetcode/p490/Solution.java
@@ -57,6 +57,9 @@ package com.org.lengchuan.algorithm.leetcode.p490;
 //
 // Related Topics 深度优先搜索 广度优先搜索
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author lengchuan <lishuijun1992@gmail.com>
  * @date 2020/7/31
@@ -66,7 +69,77 @@ public class Solution {
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
         // 广度或者深度 关键点在于每次行进时如果没有遇到墙就一直往前走，直到遍历所有路径
+        // 每次遇到墙之后，看当前位置是否已经走过，如果没有走过，则从当前位置开始继续走，同时把当前位置标记为已经走过
+        if (maze.length == 0) {
+            return false;
+        }
+        if (start[0] == destination[0] && start[1] == destination[1]) {
+            return true;
+        }
 
+        int w = maze.length;
+        int h = maze[0].length;
+        // 上 下 左 右 四个方向
+        int[][] direct = new int[][]{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        boolean[][] visited = new boolean[w][h];
+        visited[start[0]][start[1]] = true;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(start[0], start[1]));
+
+        while (!queue.isEmpty()) {
+            Pair pair = queue.poll();
+            if (pair.x == destination[0] && pair.y == destination[1]) {
+                return true;
+            }
+
+            // 四个方向遍历
+            for (int[] d : direct) {
+                int x = pair.x + d[0];
+                int y = pair.y + d[1];
+                // 没有遇到墙之前一直往前走
+                while (x >= 0 && y >= 0 && x < w && y < h && maze[x][y] == 0) {
+                    x = x + d[0];
+                    y = y + d[1];
+                }
+
+                // 遇到墙之后，如果当前位置没有访问过，则从当前位置开始继续往下走
+                if (!visited[x - d[0]][y - d[1]]) {
+                    visited[x - d[0]][y - d[1]] = true;
+                    queue.offer(new Pair(x - d[0], y - d[1]));
+                }
+            }
+
+        }
         return false;
+    }
+
+    private static class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+        //0 0 1 0 0
+        //0 0 0 0 0
+        //0 0 0 1 0
+        //1 1 0 1 1
+        //0 0 0 0 0
+
+        int[][] maze = new int[][]{
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {1, 1, 0, 1, 1},
+                {0, 0, 0, 0, 0}
+        };
+
+        int[] statr = new int[]{0, 4};
+        int[] destination = new int[]{4, 4};
+        System.out.println(new Solution().hasPath(maze, statr, destination));
     }
 }
